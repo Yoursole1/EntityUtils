6d81ad6f2fbf606268b268b6b37a631f0fd14e1d
Owen V. Hayes <marvelousanything@gmail.com>
2022-08-07 22:43:36 -0700

    Fixed plugin initialization error.

--- src/main/java/org/entityutils/EntityUtilsPlugin.java
+++ src/main/java/org/entityutils/EntityUtilsPlugin.java
@@ -5,11 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class EntityUtilsPlugin extends JavaPlugin {
 
     // Make this class a singleton
-    private static EntityUtilsPlugin instance = new EntityUtilsPlugin();
+    private static EntityUtilsPlugin instance;
 
     @Override
     public void onEnable() {
         getLogger().info("EntityUtils enabled!");
+        instance = this;
     }
 
     @Override
