0774a51ffd122dd35d8ef0f0979b374794ed0b5e
joshua_beard <matinzzz@icloud.com>
2022-11-09 08:55:31 -0800

    .

--- src/main/java/org/entityutils/entity/npc/player/AbstractPlayerNPC.java
+++ src/main/java/org/entityutils/entity/npc/player/AbstractPlayerNPC.java
@@ -16,6 +16,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.pathfinder.PathFinder;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
