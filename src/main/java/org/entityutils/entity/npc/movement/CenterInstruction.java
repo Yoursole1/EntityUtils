ea03b0d008ae3f852df180ad67d47804f7c385b0
joshua_beard <matinzzz@icloud.com>
2022-11-24 10:47:11 -0800

    working commit (not done)

--- src/main/java/org/entityutils/entity/npc/movement/CenterInstruction.java
+++ src/main/java/org/entityutils/entity/npc/movement/CenterInstruction.java
@@ -7,9 +7,21 @@ import java.util.List;
 /**
  * Centers the NPC on its block
  */
-public class CenterInstruction implements Instruction{
+public class CenterInstruction implements Instruction {
+
+    private final Vector3 currentLocation;
+
+    public CenterInstruction(Vector3 currentLocation){
+        this.currentLocation = currentLocation;
+    }
+
     @Override
     public List<Vector3> generateMovementVectors() {
-        return null;
+        Vector3 floor = new Vector3(Math.floor(this.currentLocation.getX()), this.currentLocation.getY(), Math.floor(this.currentLocation.getZ()));
+        Vector3 center = new Vector3(floor.getX() + 0.5D, floor.getY(), floor.getZ() + 0.5D);
+
+        WalkInstruction ins = new WalkInstruction(currentLocation, center, 1);
+
+        return ins.generateMovementVectors();
     }
 }
