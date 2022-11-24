aa0c0be523d166e44f57f90bd55b71a4aadacee6
Yoursole1 <matinzzz@icloud.com>
2022-11-23 22:58:14 -0800

    added instructions

---
+++ src/main/java/org/entityutils/entity/npc/movement/CenterInstruction.java
@@ -0,0 +1,15 @@
+package org.entityutils.entity.npc.movement;
+
+import org.entityutils.utils.math.Vector3;
+
+import java.util.List;
+
+/**
+ * Centers the NPC on its block
+ */
+public class CenterInstruction implements Instruction{
+    @Override
+    public List<Vector3> generateMovementVectors() {
+        return null;
+    }
+}
---
+++ src/main/java/org/entityutils/entity/npc/movement/TeleportInstruction.java
@@ -0,0 +1,17 @@
+package org.entityutils.entity.npc.movement;
+
+import org.entityutils.utils.math.Vector3;
+
+import java.util.List;
+
+/**
+ * Teleports the NPC
+ *
+ * Might not work well with the current Instruction system
+ */
+public class TeleportInstruction implements Instruction{
+    @Override
+    public List<Vector3> generateMovementVectors() {
+        return null;
+    }
+}
