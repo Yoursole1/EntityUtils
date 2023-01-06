5d1d953fef859da07a3de72a138c0ded2a931e44
joshua_beard <matinzzz@icloud.com>
2023-01-06 15:14:32 -0800

    matrix work

---
+++ src/main/java/org/entityutils/utils/math/linearAlg/Operable.java
@@ -0,0 +1,12 @@
+package org.entityutils.utils.math.linearAlg;
+
+
+public interface Operable {
+
+    Operable add(Operable other);
+    Operable multiply(Operable other);
+
+    int[] getDimension();
+
+    Operable[] getGroup(int location, boolean v);
+}
