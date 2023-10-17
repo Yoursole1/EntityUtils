1db8ad3cc71356a5a1fc5fc677db79eaf24efe4e
Yoursole <80355246+Yoursole1@users.noreply.github.com>
2023-10-16 20:10:39 -0700

    .

--- src/main/java/org/entityutils/Test.java
+++ src/main/java/org/entityutils/Test.java
@@ -14,25 +14,18 @@ import java.util.UUID;
 
 public class Test implements Listener {
 
-    static List<AnimatedPlayerNPC> npcs = new ArrayList<>();
+    static AnimatedPlayerNPC npc;
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
-        npcs.add(new AnimatedPlayerNPC("Hi world", e.getPlayer().getLocation(), EntityUtilsPlugin.getInstance()));
-        npcs.get(npcs.size()-1).setAlive(true);
-        npcs.get(npcs.size()-1).showName(true);
-        npcs.get(npcs.size()-1).setSkin(UUID.fromString("8c13f015-b6fa-4752-9f8b-02629addbf98"), SkinLayer.HAT);
+        npc = new AnimatedPlayerNPC("Hi world", e.getPlayer().getLocation(), EntityUtilsPlugin.getInstance());
+        npc.setAlive(true);
     }
 
     private boolean show = false;
     @EventHandler
     public void onShift(PlayerToggleSneakEvent e) {
-        if (e.isSneaking()){
-            return;
-        }
-        for(AnimatedPlayerNPC npc : npcs){
-            npc.goTo(e.getPlayer().getLocation(), 8);
-        }
+        npc.setDirection(npc.getData().getYaw(), 90);
 
     }
     @EventHandler
